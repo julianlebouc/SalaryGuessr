0a819c2e32d51f807e8302b5980aea30b4e66a1d
julianlebouc <flagadortv@gmail.com>
2026-05-02 21:19:34 +0200

    Modified sound effects for a more enjoyable experience

--- frontend/src/sound/SoundProvider.jsx
+++ frontend/src/sound/SoundProvider.jsx
@@ -60,24 +60,42 @@ export function SoundProvider({ children }) {
       if (volume <= 0) return;
       const audioCtx = await ensureAudioContext();
       if (!audioCtx) return;
-
+  
       switch (kind) {
         case "roundEnd":
-          playTone(audioCtx, { frequency: 620, volume: 0.7 * volume, duration: 0.08, type: "triangle" });
-          playTone(audioCtx, { frequency: 740, volume: 0.6 * volume, duration: 0.1, type: "triangle", delay: 0.08 });
+          playTone(audioCtx, { frequency: 523.25, volume: 0.9 * volume, duration: 0.08, type: "sine" });
+          playTone(audioCtx, { frequency: 659.25, volume: 0.9 * volume, duration: 0.08, type: "sine", delay: 0.08 });
+          playTone(audioCtx, { frequency: 783.99, volume: 0.9 * volume, duration: 0.12, type: "sine", delay: 0.16 });
+          playTone(audioCtx, { frequency: 1046.50, volume: 1.0 * volume, duration: 0.25, type: "sine", delay: 0.28 });
+          playTone(audioCtx, { frequency: 1318.52, volume: 0.95 * volume, duration: 0.3, type: "sine", delay: 0.53 });
           break;
+          
         case "gameEnd":
-          playTone(audioCtx, { frequency: 523, volume: 0.9 * volume, duration: 0.12, type: "triangle" });
-          playTone(audioCtx, { frequency: 659, volume: 0.85 * volume, duration: 0.12, type: "triangle", delay: 0.1 });
-          playTone(audioCtx, { frequency: 784, volume: 0.8 * volume, duration: 0.14, type: "triangle", delay: 0.2 });
+          playTone(audioCtx, { frequency: 523.25, volume: 0.85 * volume, duration: 0.2, type: "sine" });
+          playTone(audioCtx, { frequency: 659.25, volume: 0.85 * volume, duration: 0.2, type: "sine", delay: 0.2 });
+          playTone(audioCtx, { frequency: 783.99, volume: 0.85 * volume, duration: 0.2, type: "sine", delay: 0.4 });
+          playTone(audioCtx, { frequency: 1046.50, volume: 1.0 * volume, duration: 0.35, type: "sine", delay: 0.6 });
+          playTone(audioCtx, { frequency: 1318.52, volume: 0.9 * volume, duration: 0.3, type: "sine", delay: 0.95 });
+          playTone(audioCtx, { frequency: 1567.98, volume: 0.9 * volume, duration: 0.35, type: "sine", delay: 1.25 });
+          playTone(audioCtx, { frequency: 2093.00, volume: 1.0 * volume, duration: 0.5, type: "sine", delay: 1.6 });
+          playTone(audioCtx, { frequency: 2637.02, volume: 0.85 * volume, duration: 0.4, type: "sine", delay: 2.1 });
           break;
+          
         case "success":
-          playTone(audioCtx, { frequency: 690, volume: 0.65 * volume, duration: 0.06, type: "sine" });
-          playTone(audioCtx, { frequency: 860, volume: 0.65 * volume, duration: 0.07, type: "sine", delay: 0.05 });
+          playTone(audioCtx, { frequency: 1046.50, volume: 0.8 * volume, duration: 0.06, type: "sine" });
+          playTone(audioCtx, { frequency: 1318.52, volume: 0.8 * volume, duration: 0.08, type: "sine", delay: 0.06 });
+          playTone(audioCtx, { frequency: 1567.98, volume: 0.85 * volume, duration: 0.1, type: "sine", delay: 0.14 });
+          playTone(audioCtx, { frequency: 2093.00, volume: 0.9 * volume, duration: 0.15, type: "sine", delay: 0.24 });
           break;
+          
         case "error":
-          playTone(audioCtx, { frequency: 260, volume: 0.6 * volume, duration: 0.08, type: "square" });
+          playTone(audioCtx, { frequency: 440.00, volume: 0.7 * volume, duration: 0.12, type: "sawtooth" });
+          playTone(audioCtx, { frequency: 349.23, volume: 0.65 * volume, duration: 0.12, type: "sawtooth", delay: 0.12 });
+          playTone(audioCtx, { frequency: 261.63, volume: 0.6 * volume, duration: 0.15, type: "sawtooth", delay: 0.24 });
+          playTone(audioCtx, { frequency: 196.00, volume: 0.55 * volume, duration: 0.2, type: "sawtooth", delay: 0.39 });
+          playTone(audioCtx, { frequency: 130.81, volume: 0.5 * volume, duration: 0.3, type: "sawtooth", delay: 0.59 });
           break;
+          
         case "click":
         default:
           playTone(audioCtx, { frequency: 480, volume: 0.45 * volume, duration: 0.04, type: "sine" });
